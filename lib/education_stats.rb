f07da7d39364d9a20d6ba8e9937444ac1361e7bb
Chris Ewald <chrisewald@gmail.com>
2016-03-29 12:42:20 -0400

    add the client

--- lib/education_stats.rb
+++ lib/education_stats.rb
@@ -1,5 +1,80 @@
 require "education_stats/version"
+require "education_stats/client"
+
+require 'statsd-ruby'
 
 module EducationStats
-  # Your code goes here...
+  HOSTED_GRAPHITE_HOST = 'statsd.hostedgraphite.com'.freeze
+  HOSTED_GRAPHITE_PORT = 8125.freeze
+
+  class << self
+    attr_accessor :hosted_graphite_api_key
+    attr_writer   :use_hosted_graphite
+
+    # Public: Configuration for EducationStats
+    #
+    # Example
+    #
+    #   EducationStats.configure do |config|
+    #     config.hosted_graphite_api_key = 'abc123'
+    #   end
+    #
+    # Yields self for configuring options
+    def configure
+      yield self
+    end
+
+    # Public: Get the configured EducationStats client. This acts as a single
+    # Statd client, but will forward all commands to the internal clients.
+    #
+    # Example
+    #
+    #   client = EducationStats.client
+    #   client.increment 'education.mymetric'
+    #
+    # Returns a configured instance of EducationStats::Client
+    def client
+      EducationStats::Client.new(all_clients)
+    end
+
+    # Public: Add an instance of Statsd::Client to report to.
+    def add_statsd_client(statsd_client)
+      statsd_clients << statsd_client
+    end
+
+    # Public: Get all the configured Statsd clients that will be reported to.
+    #
+    # Returns an array of Statsd client classes
+    def all_clients
+      @all_clients ||= if use_hosted_graphite
+        statsd_clients.push(build_hosted_graphite_statsd_client)
+      else
+        statsd_clients
+      end
+    end
+
+    # Public: should EducationStats use a HostedGraphite Statsd
+    #
+    # Returns a Boolean
+    def use_hosted_graphite
+      @use_hosted_graphite ||= !!hosted_graphite_api_key
+    end
+
+    private
+    # Internal: Returns an array of manually added clients.
+    #
+    # Returns an array of Statsd client classes
+    def statsd_clients
+      @statsd_clients ||= []
+    end
+
+    # Internal: Builds an Statsd client for HostedGraphite.
+    #
+    # Returns a Statsd class configured for HostedGraphite
+    def build_hosted_graphite_statsd_client
+      Statsd.new(HOSTED_GRAPHITE_HOST, HOSTED_GRAPHITE_PORT).tap do |statsd|
+        statsd.namespace = hosted_graphite_api_key
+      end
+    end
+  end
 end
---
+++ lib/education_stats/client.rb
@@ -0,0 +1,40 @@
+module EducationStats
+  # EducationStats::Client is responsible for forwarding Statsd commands to a
+  # list of Statd clients. Every command will be forwarded to ALL clients,
+  # making this a very distinct concept from Statd shards / sharding which
+  # distributes the call load over multiple shards. Multiple shards may still be
+  # used in a Statsd instance added through the add 'add_statsd_client' method.
+  class Client
+    attr_reader :all_clients
+
+    # A list of forwarding methods to build
+    FORWARD_METHODS = [
+      :increment,
+      :decrement,
+      :count,
+      :guage,
+      :timing,
+      :time
+    ].freeze
+
+    # Public: Constructor for Client class.
+    #
+    # clients - An array of configured Statd client classes. Commands will be
+    # forwarded to each of these clients.
+    #
+    # Returns a EducationStats::Client instance
+    def initialize(clients)
+      @all_clients = clients
+    end
+
+    # Public: Statsd command methods. Commands and all arguments are simply
+    # forwarded to every Statsd client.
+    #
+    # Returns an array of return values from the command called on each client.
+    FORWARD_METHODS.each do |method|
+      define_method method do |*args|
+        @all_clients.map { |client| client.send(method, *args) }
+      end
+    end
+  end
+end
